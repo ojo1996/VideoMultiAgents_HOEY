69e378d2ec15937c8248bdc6a640d4aa20a2113f
kugou-noriyuki <kugou.noriyuki@jp.panasonic.com>
2025-01-28 15:39:17 +0000

    update stage1 to using langchain

--- util.py
+++ util.py
@@ -251,41 +251,6 @@ def post_process(response):
         return -1
 
 
-def extract_expert_info_json(data):
-    result = {}
-
-    json_start = data.find('{')
-    json_end = data.rfind('}') + 1
-
-    json_data = data[json_start:json_end]
-
-    if json_start != -1:
-        try:
-            json_extract = json.loads(json_data)
-            for key, value in json_extract.items():
-                if "ExpertName" in key and "Prompt" not in key:
-                    number = re.findall(r'\d+', key)[0]
-                    expert_key = f"ExpertName{number}"
-                    prompt_key = f"ExpertName{number}Prompt"
-                    expert_name = value
-                    prompt_text = json_extract.get(prompt_key, "")
-
-                    result[expert_key] = expert_name.strip().replace('"', "'")
-                    result[prompt_key] = prompt_text.strip().replace('"', "'")
-        except json.JSONDecodeError:
-            print("JSONDecodeError: Failed to extract expert information from the response.")
-
-    return result
-
-
-def extract_expert_info(data):
-    result = extract_expert_info_json(data)
-    if len([k for k in result if "ExpertName" in k]) >= 2 and len([k for k in result if "Prompt" in k]) >= 3:
-        return result
-    else:
-        return None
-
-
 def read_json_file(file_path):
     # print ("read_json_file")
     try:
