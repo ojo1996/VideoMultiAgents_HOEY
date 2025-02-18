c09dcbd9c745720ac458b95b09d7090042523f36
ashishguptajp <gupta.ashish@jp.panasonic.com>
2025-02-18 19:43:44 +0900

    SAS experiments on NExT-QA

--- azure-batch/main.py
+++ azure-batch/main.py
@@ -1,5 +1,3 @@
-print("Starting the main script........")
-
 import os
 import sys
 import json
@@ -10,17 +8,11 @@ from stage1 import execute_stage1
 from stage2 import execute_video_question_answering
 # from util import get_video_summary
 
-
-print("Starting the main script...")
-
 # Check the environment variables
 env_vars = [ "BLOB_CONNECTION_STRING", "COSMOS_CONNECTION_STRING", "EXPERIMENT_ID", "DATASET", "OPENAI_API_KEY", "QUESTION_ID", "VIDEO_FILE_NAME", "QA_JSON_STR" ]
 for var in env_vars:
     value = os.getenv(var, "Not Set")
     print(f"{var}: {value}")
-
-
-print("Environment variables checked.")
 
 # Set the environment variables
 os.environ["IMAGES_DIR_PATH"] = "/root/VideoMultiAgents/images"
@@ -35,7 +27,7 @@ elif os.getenv("DATASET") == "nextqa":
     os.environ["VIDEOTREE_RESULTS_PATH"]  = "/root/VideoMultiAgents/nextqa_videotree_result.json"
     os.environ["SUMMARY_CACHE_JSON_PATH"] = "/root/VideoMultiAgents/nextqa_summary_cache.json"
     # os.environ["SUMMARY_INFO"]            = json.dumps(get_video_summary(os.getenv("SUMMARY_CACHE_JSON_PATH"), os.getenv("VIDEO_FILE_NAME")))
-    os.environ["FRAME_NUM"]               = str(32)
+    os.environ["FRAME_NUM"]               = str(90)
 elif os.getenv("DATASET") == "momaqa":
     os.environ["CAPTIONS_FILE"]           = "/root/VideoMultiAgents/momaqa_captions.json"
     os.environ["VIDEOTREE_RESULTS_PATH"]  = "/root/VideoMultiAgents/momaqa_videotree_result.json"
@@ -45,7 +37,6 @@ elif os.getenv("DATASET") == "momaqa":
     # os.environ["SUMMARY_INFO"]            = json.dumps(get_video_summary(os.getenv("SUMMARY_CACHE_JSON_PATH"), os.getenv("VIDEO_FILE_NAME")))
     os.environ["FRAME_NUM"]               = str(90)
 
-print("Downloaded necessary files .")
 
 # Download images and othere necessary files
 download_blob_data(os.getenv("BLOB_CONNECTION_STRING"), os.getenv("VIDEO_FILE_NAME"), os.getenv("IMAGES_DIR_PATH"))
