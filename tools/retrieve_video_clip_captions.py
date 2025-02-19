35d773cfe25a1a2b9b90470b7805406f68030da9
ashishguptajp <gupta.ashish@jp.panasonic.com>
2025-02-19 16:28:42 +0900

    Update for SAS NExTQA experiments

--- tools/retrieve_video_clip_captions.py
+++ tools/retrieve_video_clip_captions.py
@@ -31,10 +31,8 @@ def retrieve_video_clip_captions() -> list[str]:
         captions_data = json.load(f)
 
     captions = captions_data.get(video_filename, [])
-    print("Captions1: ", captions)
+    print("Captions: ", captions)
 
-    captions = captions_data[video_filename]
-    print("Captions2: ", captions)
     result = []
     previous_caption = None
 
